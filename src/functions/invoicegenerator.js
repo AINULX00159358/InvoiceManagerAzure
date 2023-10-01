13e8210d09ed798c491b22e9961ca088e16883c6
x00159358 <x00159358@mytudublin.ie>
2023-10-01 01:30:20 +0100

    renamed app as invoicegenetator

--- src/functions/invoicegenerator.js
+++ src/functions/invoicegenerator.js
@@ -1,6 +1,6 @@
 const { app } = require('@azure/functions');
 
-app.eventGrid('eventGridTrigger1', {
+app.eventGrid('invoicegenerator', {
     handler: (event, context) => {
         context.log('Event grid function processed event:', event);
     }
