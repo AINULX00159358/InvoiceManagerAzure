712d56dc0602fb0fa704013756bc1e39233525c6
x00159358 <x00159358@mytudublin.ie>
2023-10-01 10:47:56 +0100

    renoced common

--- src/functions/invoicegenerator.js
+++ src/functions/invoicegenerator.js
@@ -1,17 +1,17 @@
-import { newInvoice, createJsonResponse } from '../common/common.js';
+//import { newInvoice, createJsonResponse } from '../common/common.js';
 import { app, output } from '@azure/functions';
 
-const eventGridOutput = output.eventGrid({
-    topicEndpointUri: 'InvMgrEventGridTopicUri',
-    topicKeySetting: 'InvMgrEventGridTopicKey',
-});
+// const eventGridOutput = output.eventGrid({
+//     topicEndpointUri: 'InvMgrEventGridTopicUri',
+//     topicKeySetting: 'InvMgrEventGridTopicKey',
+// });
 
 app.eventGrid('invoicegenerator', {
-    return: eventGridOutput,
+  //  return: eventGridOutput,
     handler: (event, context) => {
         console.log('Event grid function processed event:'+ event);
-        const invoice = newInvoice(event.data.custID, event.data.amount);
-        return createJsonResponse(invoice);
+      //  const invoice = newInvoice(event.data.custID, event.data.amount);
+      //  return createJsonResponse(invoice);
     }
 });
 
