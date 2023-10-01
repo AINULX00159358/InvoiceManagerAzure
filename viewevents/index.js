7836df155b5b3f4dc92c959fabb8db25d3bf1a57
x00159358 <x00159358@mytudublin.ie>
2023-10-01 14:42:58 +0100

    added event viewer

---
+++ viewevents/index.js
@@ -0,0 +1,7 @@
+const common = require('../common/common.js')
+console.log("common ", common)
+
+
+module.exports = async function (context, eventGridEvent) {
+    context.log("Event ", eventGridEvent.type + " :: "+JSON.stringify(eventGridEvent.data));
+};
