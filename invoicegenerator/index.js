7711ad658598b30474ff13f490d92e4f6399f0a7
x00159358 <x00159358@mytudublin.ie>
2023-10-01 14:20:20 +0100

    added return

--- invoicegenerator/index.js
+++ invoicegenerator/index.js
@@ -3,9 +3,11 @@ console.log("common ", common)
 
 
 module.exports = async function (context, eventGridEvent) {
-    context.log(typeof eventGridEvent);
+    
     context.log("--------------------------------------------------------------")
     context.log("Data received " + JSON.stringify(eventGridEvent.data));
     context.log("--------------------------------------------------------------")
-    
+    const invoice = common.newInvoice(eventGridEvent.data.custID, eventGridEvent.data.amount);
+    context.log(" returning  "+ JSON.stringify(invoice));
+    return common.createCloudEvent(invoice);
 };
