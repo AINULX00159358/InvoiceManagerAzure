8a1db419e47e3d3c11f5faa2af7e161847f44091
x00159358 <x00159358@mytudublin.ie>
2023-10-28 20:47:32 +0100

    reveretd auditreporter

--- auditreporter/index.js
+++ auditreporter/index.js
@@ -1,16 +1,17 @@
 let maxLatency = 0
-//let counter = 0
-//let sumlatency = 0
+let counter = 0
+let sumlatency = 0
 
 
 module.exports = async function (context, eventGridEvent) {
-  //counter = counter + 1;
+  counter = counter + 1;
   let starting = eventGridEvent.data.history["invoice_NEW"];
   let ending = eventGridEvent.data.history["invoice_CLOSED"];
   let latency = ending - starting;
   if (latency > maxLatency) {
     maxLatency = latency;
   }
-  context.log(eventGridEvent.data.invoiceID + ", Count="+counter + ", current="+ latency+ ", Max="+ maxLatency);
-  
+  sumlatency = sumlatency + latency
+  let avgLatency = sumlatency / counter;
+  context.log(eventGridEvent.data.invoiceID + ", Count="+counter, ", avgerage ="+ avgLatency+ ", current="+ latency+ ", Max="+ maxLatency);
 };
